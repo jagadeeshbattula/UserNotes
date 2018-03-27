6dbcb168d0690ef7fa8f285df1c696bb8826e05d
jagadeeshgoftx <jagadeesh@goftx.com>
2018-03-27 17:01:29 -0400

    Added Signup, Validation, Hashing, Entity FrameWork Code Refactoring

--- LogIn.xaml.cs
+++ LogIn.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
 
 namespace UserNotes
 {
@@ -12,36 +14,188 @@ namespace UserNotes
     /// </summary>
     public partial class LogIn : Page
     {
-        WPFApplicationEntities _db = new WPFApplicationEntities();
+        //Entity Frame Work class
+        WPFApplicationEntities _db  = new WPFApplicationEntities();
+        //Hash key for Encoding password
+        public static string key    = "usernoteapplication";
+
         /*
          * Constructor for LogIn
          */
         public LogIn()
         {
             InitializeComponent();
+
+            btnLogin.Visibility             = Visibility.Hidden;
+            lblTitle.Content                = "Log In Here";
+            lblConformPassword.Visibility   = Visibility.Hidden;
+            txtConformPassword.Visibility   = Visibility.Hidden;
+            btnSignupSubmit.Visibility      = Visibility.Hidden;
+        }
+
+        /*
+         * btnLogin_Click event
+         * 
+         * Enables log in controlles of Log in form and disable Signup controlles
+         */
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            btnLogin.Visibility             = Visibility.Hidden;
+            btnSignup.Visibility            = Visibility.Visible;
+
+            lblTitle.Content                = "Log In Here";
+
+            lblConformPassword.Visibility   = Visibility.Hidden;
+            txtConformPassword.Visibility   = Visibility.Hidden;
+
+            btnLoginSubmit.Visibility       = Visibility.Visible;
+            btnSignupSubmit.Visibility      = Visibility.Hidden;
+
+        }
+
+        /*
+         * btnSignup_Click event
+         * 
+         * Enables Sign up controlles of Log in form and disable Log in controlles
+         */
+        private void btnSignup_Click(object sender, RoutedEventArgs e)
+        {
+            btnSignup.Visibility            = Visibility.Hidden;
+            btnLogin.Visibility             = Visibility.Visible;
+
+            lblTitle.Content                = "Sign Up Here";
+
+            lblConformPassword.Visibility   = Visibility.Visible;
+            txtConformPassword.Visibility   = Visibility.Visible;
+
+            btnLoginSubmit.Visibility       = Visibility.Hidden;
+            btnSignupSubmit.Visibility      = Visibility.Visible;
         }
 
         /* 
-         * btnSubmit_Click event
+         * CheckForValidation function
          * 
-         * btnSbumit click event for checking log in credentials
+         * For checking input feilds validation
          */
-        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        public bool CheckForValidation()
         {
+            bool allGood = false;
 
-            int id = (from user in _db.tblUsers
-                      where user.username == txtUsername.Text && user.password == txtPassword.Text
-                      select user.id).SingleOrDefault();
-            
-            if (id != 0)
+            if(txtUsername.Text == null || txtPassword.Password == null || txtUsername.Text.Trim().Length == 0 || txtPassword.Password.Trim().Length == 0)
             {
-                UserHome userHome = new UserHome(id);
-                this.NavigationService.Navigate(userHome, id);
+                allGood = false;
             }
             else
             {
-                MessageBox.Show("User name and/or Password do not match !");
+                allGood = true;
             }
+
+            return allGood;
+        }
+
+        /* 
+         * btnLoginSubmit_Click event
+         * 
+         * Event for checking log in credentials
+         */
+        private void btnLoginSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if(!CheckForValidation())
+            {
+                MessageBox.Show("User Name and Password feilds required..!");
+            }
+            else
+            {
+                string password = Encrypt(txtPassword.Password);
+
+                int id = (from user in _db.tblUsers
+                          where user.username == txtUsername.Text && user.password == password
+                          select user.id).SingleOrDefault();
+
+                if (id != 0)
+                {
+                    UserHome userHome = new UserHome(id);
+
+                    this.NavigationService.Navigate(userHome, id);
+                }
+                else
+                {
+                    MessageBox.Show("User name and/or Password do not match !");
+                }
+            }
+        }
+
+        /* 
+         * btnSignupSubmit_Click event
+         * 
+         * Event for creating new user in DataBase
+         */
+        private void btnSignupSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckForValidation() || txtConformPassword.Password == "" || txtConformPassword.Password.Trim().Length == 0)
+            {
+                MessageBox.Show("User Name, Password and Conform Password feilds required..!");
+            }
+            else
+            {
+                if(txtPassword.Password != txtConformPassword.Password )
+                {
+                    MessageBox.Show("Password and Conform Password feilds do not match");
+                }
+                else
+                {
+                    string password                 = Encrypt(txtPassword.Password);
+
+                    tblUser user = new tblUser()
+                    {
+                        username = txtUsername.Text,
+                        password = password
+                    };
+
+                    _db.tblUsers.Add(user);
+                    _db.SaveChanges();
+
+                    btnLogin.Visibility             = Visibility.Hidden;
+                    btnSignup.Visibility            = Visibility.Visible;
+
+                    lblTitle.Content                = "Log In Here";
+
+                    lblConformPassword.Visibility   = Visibility.Hidden;
+                    txtConformPassword.Visibility   = Visibility.Hidden;
+
+                    btnLoginSubmit.Visibility       = Visibility.Visible;
+                    btnSignupSubmit.Visibility      = Visibility.Hidden;
+                }
+            }
+        }
+
+        /* 
+         * Encrypt function
+         * 
+         * Encrypts password with hash key
+         */
+        public string Encrypt(string password)
+        {
+            byte[] bytesBuff = Encoding.Unicode.GetBytes(password);
+
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes crypto   = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key                     = crypto.GetBytes(32);
+                aes.IV                      = crypto.GetBytes(16);
+
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                        cStream.Close();
+                    }
+
+                    password = System.Convert.ToBase64String(mStream.ToArray());
+                }
+            }
+            return password;
         }
     }
 }
--- UserHome.xaml.cs
+++ UserHome.xaml.cs
@@ -13,8 +13,9 @@ namespace UserNotes
     /// </summary>
     public partial class UserHome : Page
     {
-
+        //Entity Frame Work class
         WPFApplicationEntities _db = new WPFApplicationEntities();
+        
         /* 
          * Constructor for UserHome
          */
@@ -33,6 +34,7 @@ namespace UserNotes
             if(id != 0)
             {
                 txtUserId.Text = id.ToString();
+                txtViewNote.Visibility = Visibility.Hidden;
                 ViewUserNotes(id);
             }
         }
@@ -44,61 +46,79 @@ namespace UserNotes
          */
         private void ViewUserNotes(int id)
         {
-            btnSubmitUpdateNote.IsEnabled = false;
-            btnSubmitSaveNote.IsEnabled = true;
-            gridUserNotes.ItemsSource = _db.tblNotes.Where(n => n.user_id == id).ToList();
-        }
+            btnSubmitUpdateNote.IsEnabled   = false;
+            btnSubmitSaveNote.IsEnabled     = true;
 
-        private void deleteBtn_Click(object sender, RoutedEventArgs e)
-        {
-            int userId = int.Parse(txtUserId.Text);
-            int id = (gridUserNotes.SelectedItem as tblNote).id;
-            var deleteNote = _db.tblNotes.Where(m => m.id == id).Single();
-            _db.tblNotes.Remove(deleteNote);
-            _db.SaveChanges();
-            gridUserNotes.ItemsSource = _db.tblNotes.Where(n => n.user_id == userId).ToList();
-        }
+            gridUserNotes.ItemsSource       = _db.tblNotes.Where(n => n.user_id == id).ToList();
 
-        private void updateBtn_Click(object sender, RoutedEventArgs e)
-        {
-            txtNewNote.Text = (gridUserNotes.SelectedItem as tblNote).note;
-            btnSubmitUpdateNote.IsEnabled = true;
-            btnSubmitSaveNote.IsEnabled = false;
-        }
-
-        private void btnSubmitUpdateNote_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtNewNote.Text == " " || txtNewNote.Text == "")
-            {
-                MessageBox.Show("Please enter note before saving !");
-            }
-            else
-            {
-                int userId = int.Parse(txtUserId.Text);
-
-                int id = (gridUserNotes.SelectedItem as tblNote).id;
-
-                tblNote updateNote = (from n in _db.tblNotes where n.id == id select n).Single();
-                updateNote.user_id = userId;
-                updateNote.note = txtNewNote.Text;
-
-                _db.SaveChanges();
-                gridUserNotes.ItemsSource = _db.tblNotes.Where(n => n.user_id == userId).ToList();
-                btnSubmitUpdateNote.IsEnabled = false;
-                btnSubmitSaveNote.IsEnabled = true;
-                txtNewNote.Text = "";
-            }
-            
         }
 
         /* 
-        * btnSaveNote_Click event
+         * deleteBtn_Click event
+         * 
+         * Delete the selected note from DataBase
+         */
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int userId      = int.Parse(txtUserId.Text);
+            int id          = (gridUserNotes.SelectedItem as tblNote).id;
+            var deleteNote  = _db.tblNotes.Where(m => m.id == id).Single();
+
+            _db.tblNotes.Remove(deleteNote);
+            _db.SaveChanges();
+
+            gridUserNotes.ItemsSource = _db.tblNotes.Where(n => n.user_id == userId).ToList();
+        }
+
+        /* 
+         * updateBtn_Click event
+         * 
+         * Populate the selected note to TextBox txtNewNote for updating
+         */
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtNewNote.Text                 = (gridUserNotes.SelectedItem as tblNote).note;
+            btnSubmitUpdateNote.IsEnabled   = true;
+            btnSubmitSaveNote.IsEnabled     = false;
+        }
+
+        /* 
+         * btnSubmitUpdateNote_Click event
+         * 
+         * Update the note and save it to DataBase
+         */
+        private void btnSubmitUpdateNote_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNewNote.Text.Trim().Length == 0 || txtNewNote.Text == "")
+            {
+                MessageBox.Show("Please enter note before updating !");
+            }
+            else
+            {
+                int userId          = int.Parse(txtUserId.Text);
+                int id              = (gridUserNotes.SelectedItem as tblNote).id;
+
+                tblNote updateNote  = (from n in _db.tblNotes where n.id == id select n).Single();
+                updateNote.user_id  = userId;
+                updateNote.note     = txtNewNote.Text;
+
+                _db.SaveChanges();
+
+                gridUserNotes.ItemsSource       = _db.tblNotes.Where(n => n.user_id == userId).ToList();
+                btnSubmitUpdateNote.IsEnabled   = false;
+                btnSubmitSaveNote.IsEnabled     = true;
+                txtNewNote.Text                 = "";
+            }
+        }
+
+        /* 
+        * btnSubmitSaveNote_Click event
         * 
-        * SaveNote button click function to save new note to database
+        * Save new note to database
         */
         private void btnSubmitSaveNote_Click(object sender, RoutedEventArgs e)
         {
-            if (txtNewNote.Text == " " || txtNewNote.Text == "")
+            if (txtNewNote.Text.Trim().Length == 0 || txtNewNote.Text == "")
             {
                 MessageBox.Show("Please enter note before saving !");
             }
@@ -106,19 +126,51 @@ namespace UserNotes
             {
                 tblNote newNote = new tblNote()
                 {
-                    user_id = int.Parse(txtUserId.Text),
-                    note = txtNewNote.Text,
-                    created_at = DateTime.Now
+                    user_id     = int.Parse(txtUserId.Text),
+                    note        = txtNewNote.Text,
+                    created_at  = DateTime.Now
                 };
 
                 _db.tblNotes.Add(newNote);
                 _db.SaveChanges();
 
-                int id = int.Parse(txtUserId.Text);
-                gridUserNotes.ItemsSource = _db.tblNotes.Where(n => n.user_id == id).ToList();
-                btnSubmitUpdateNote.IsEnabled = false;
-                btnSubmitSaveNote.IsEnabled = true;
-                txtNewNote.Text = "";
+                int id                          = int.Parse(txtUserId.Text);
+                gridUserNotes.ItemsSource       = _db.tblNotes.Where(n => n.user_id == id).ToList();
+                btnSubmitUpdateNote.IsEnabled   = false;
+                btnSubmitSaveNote.IsEnabled     = true;
+                txtNewNote.Text                 = "";
+            }
+        }
+
+        /* 
+        * btnLogOut_Click event
+        * 
+        * Return back to log in page
+        */
+        private void btnLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn logIn = new LogIn();
+
+            this.NavigationService.Navigate(logIn);
+        }
+
+        /* 
+        * gridUserNotes_MouseDoubleClick event
+        * 
+        * Populates longer notes body into txtViewNote TextBox for accessibility
+        */
+        private void gridUserNotes_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int lengthOfNote = ((gridUserNotes.SelectedItem as tblNote).note).Length;
+
+            if (lengthOfNote >= 80)
+            {
+                txtViewNote.Visibility  = Visibility.Visible;
+                txtViewNote.Text        = (gridUserNotes.SelectedItem as tblNote).note;
+            }
+            else
+            {
+                txtViewNote.Visibility = Visibility.Hidden;
             }
         }
     }
